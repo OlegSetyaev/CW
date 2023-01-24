34c092c0cd4eb10c0a161d75aa60d436b6b4a8a0
Oleg <FireResist88@mail.ru>
2023-01-24 14:49:17 +0300

    Добавил программу

---
+++ CW/Program.cs
@@ -0,0 +1,19 @@
+﻿
+void FindString(string[] strings)
+{
+    string[] newStr = new string[strings.Length];
+    int j = 0;
+    for (int i = 0; i < strings.Length; i++)
+    {
+        if (strings[i].Length <= 3)
+        {
+            newStr[j] = strings[i];
+            j++;
+        }
+    }
+    System.Console.WriteLine($"{string.Join(" ", strings)} --> {string.Join(" ", newStr)}");
+}
+
+System.Console.WriteLine("Введите массив строк через пробел: ");
+string[] str = Console.ReadLine().Split(' ');
+FindString(str);
